b844a4b8dce48f495980ff57472701e72d8e07d4
Blurring Shadow <976999484@qq.com>
2023-06-25 23:42:07 +0800

    build(gradle): impl WIP

--- src/main/java/org/c0nstexpr/fishology/mixin/entity/projecttile/FishingBobberEntityMixin.java
+++ src/main/java/org/c0nstexpr/fishology/mixin/entity/projecttile/FishingBobberEntityMixin.java
@@ -1,13 +1,13 @@
-package org.c0nstexpr.fishology.mixin;
+package org.c0nstexpr.fishology.mixin.entity.projecttile;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
+@Mixin(FishingBobberEntity.class)
+public class FishingBobberEntityMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
         System.out.println("This line is printed by an example mod mixin!");
