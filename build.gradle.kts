427281437fd4509b6113309172a3ad684fb2e4fa
残象之形 <976999484@qq.com>
2023-06-26 17:23:52 +0800

    build(gradle): add owo lib dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
-import com.modrinth.minotaur.TaskModrinthUpload
+import org.jetbrains.kotlin.config.LanguageVersion
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.plugin.serialization)
-    alias(libs.plugins.kotlin.lint)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktlint)
 
-    alias(libs.plugins.modrinth.minotaur)
+    alias(libs.plugins.minotaur)
     alias(libs.plugins.loom)
-    `maven-publish`
 }
 
 val modVersion: String by project
@@ -18,9 +17,11 @@ version = modVersion
 group = "org.c0nstexpr"
 
 repositories {
-    maven("https://maven.fabricmc.net/")
     mavenCentral()
     mavenLocal()
+
+    maven("https://maven.fabricmc.net/")
+    maven("https://maven.wispforest.io")
 }
 
 dependencies {
@@ -29,10 +30,27 @@ dependencies {
     modImplementation(libs.fabric.loader)
     modImplementation(libs.fabric.api)
     modImplementation(libs.fabric.kotlin)
+
+    modImplementation(libs.owo)
+    annotationProcessor(libs.owo)
+    include(libs.owo.sentinel)
+}
+
+kotlin {
+    jvmToolchain(libs.versions.jvm.map(String::toInt).get())
 }
 
 tasks {
-    val javaVersion = JavaVersion.VERSION_17
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    compileKotlin {
+        kotlinOptions {
+            allWarningsAsErrors = true
+            languageVersion = LanguageVersion.KOTLIN_1_9.versionString
+        }
+    }
 
     processResources {
         inputs.property("id", modId)
@@ -48,23 +66,10 @@ tasks {
                     "minecraft" to libs.versions.minecraft.get(),
                     "fabricApi" to libs.versions.fabric.api.get(),
                     "fabricLoader" to libs.versions.fabric.loader.get(),
-                    "fabricKotlin" to libs.versions.fabric.kotlin.get()
+                    "fabricKotlin" to libs.versions.fabric.kotlin.get(),
+                    "owo" to libs.versions.owo.get()
                 )
             )
-        }
-    }
-
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-        options.release.set(javaVersion.toString().toInt())
-    }
-
-    compileKotlin {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
-            kotlinOptions.allWarningsAsErrors = true
         }
     }
 
@@ -73,33 +78,10 @@ tasks {
     }
 
     java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
         withSourcesJar()
     }
-
-    compileKotlin {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
-        }
-    }
-    compileTestKotlin {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
-        }
-    }
-
-    withType<TaskModrinthUpload> {
-        onlyIf { System.getenv().contains("MODRINTH_TOKEN") }
-    }
 }
 
-// configure the maven publication
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
+if (System.getenv().contains("MODRINTH_TOKEN"))
+    modrinth {
     }
-}
--- src/main/java/org/c0nstexpr/fishology/mixin/entity/projecttile/FishingBobberEntityMixin.java
+++ src/main/java/org/c0nstexpr/fishology/mixin/entity/projecttile/FishingBobberEntityMixin.java
@@ -8,8 +8,4 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FishingBobberEntity.class)
 public class FishingBobberEntityMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        System.out.println("This line is printed by an example mod mixin!");
-    }
 }
