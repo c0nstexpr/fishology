5037c57041fccd62c2f8f9e5e9e93b5c91db9a8d
残象之形 <976999484@qq.com>
2023-07-03 17:31:11 +0800

    feat(fishing): implementing configuration

--- src/main/kotlin/org/c0nstexpr/fishology/FishologyMod.kt
+++ src/main/kotlin/org/c0nstexpr/fishology/FishologyMod.kt
@@ -1,27 +1,34 @@
 package org.c0nstexpr.fishology
 
+import com.badoo.reaktive.disposable.Disposable
 import com.badoo.reaktive.disposable.scope.disposableScope
 import com.badoo.reaktive.disposable.scope.doOnDispose
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import org.c0nstexpr.fishology.events.UseRodEvent
+
+val config: FishologyConfig = FishologyConfig.createAndLoad()
 
 /**
  * entry point defined in fabric.mod.json
  */
 @Suppress("unused")
 fun init() {
-    val scope = disposableScope {
-        var action: FishologyAction? = null
+    var scope: Disposable? = null
 
-        UseRodEvent.observable.subscribeScoped {
-            if (action?.arg?.equals(it) == true) return@subscribeScoped
+    ClientLifecycleEvents.CLIENT_STARTED.register {
+        scope = disposableScope {
+            var action: FishologyAction? = null
 
-            action?.dispose()
-            action = FishologyAction(it)
+            UseRodEvent.observable.subscribeScoped {
+                if (action?.arg?.equals(it) == true) return@subscribeScoped
+
+                action?.dispose()
+                action = FishologyAction(it)
+            }
+
+            doOnDispose { action?.dispose() }
         }
-
-        doOnDispose { action?.dispose() }
     }
 
-    ClientLifecycleEvents.CLIENT_STOPPING.register { scope.dispose() }
+    ClientLifecycleEvents.CLIENT_STOPPING.register { scope?.dispose() }
 }
