60ba116e7d4f5cbdc7cd731c8608b59fc0204871
Blurring Shadow <976999484@qq.com>
2023-07-04 23:56:24 +0800

    feat(fishology): implementing

--- src/main/kotlin/org/c0nstexpr/fishology/FishologyMod.kt
+++ src/main/kotlin/org/c0nstexpr/fishology/FishologyMod.kt
@@ -1,10 +1,7 @@
 package org.c0nstexpr.fishology
 
-import com.badoo.reaktive.disposable.Disposable
-import com.badoo.reaktive.disposable.scope.disposableScope
-import com.badoo.reaktive.disposable.scope.doOnDispose
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
-import org.c0nstexpr.fishology.events.UseRodEvent
+import org.c0nstexpr.fishology.disposable.eventScopedDisposable
 
 var fishology: Fishology? = null
 
@@ -13,20 +10,12 @@ var fishology: Fishology? = null
  */
 @Suppress("unused")
 fun init() {
-    var scope: Disposable? = null
-
-    ClientLifecycleEvents.CLIENT_STARTED.register {
-
-        fishology.
-    }
-
-    ClientLifecycleEvents.CLIENT_STOPPING.register { scope?.dispose() }
-}
-
-fun onUseRod(action: FishologyAction?, it: UseRodEvent.Arg)
-{
-    if (!config.enabled() || action?.arg?.equals(it) == true) return
-
-    action?.dispose()
-    FishologyAction(it)
+    eventScopedDisposable(
+        {
+            val f = Fishology()
+            f.also { fishology = it }
+        },
+        ClientLifecycleEvents.CLIENT_STARTED to { block -> ClientLifecycleEvents.ClientStarted { block() } },
+        ClientLifecycleEvents.CLIENT_STOPPING to { block -> ClientLifecycleEvents.ClientStopping { block() } }
+    )
 }
--- src/main/kotlin/org/c0nstexpr/fishology/disposable/DisposableExt.kt
+++ src/main/kotlin/org/c0nstexpr/fishology/disposable/DisposableExt.kt
@@ -1,3 +1,35 @@
 package org.c0nstexpr.fishology.disposable
 
-fun eventScopedDisposable()
+import com.badoo.reaktive.disposable.Disposable
+import com.mojang.datafixers.types.Func
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
+import net.fabricmc.fabric.api.event.Event
+
+fun <T, U> eventScopedDisposable(
+    block: () -> Disposable,
+    begin: Pair<Event<T>, (() -> Unit) -> T>,
+    end: Pair<Event<U>, (() -> Unit) -> U>
+) {
+    var disposable: Disposable? = null
+    begin.first.register(begin.second { disposable = block() })
+    end.first.register(end.second { disposable?.dispose() })
+}
+
+interface MyInterface {
+    fun doSomething()
+}
+
+fun f() {
+    val lambda = { println("Doing something!") }
+    val methodRef = MyInterface::doSomething
+
+    val myInstance = MyInterface::class.java.getMethod("invoke", Any::class.java)
+        .invoke(lambda, MyInterface::class.java)
+
+    myInstance.doSomething() // prints "Doing something!"
+
+    val myInstance2 = MyInterface::class.java.getMethod("invoke", Any::class.java)
+        .invoke(methodRef, MyInterface::class.java)
+
+    myInstance2.doSomething() // prints "Doing something!"
+}
