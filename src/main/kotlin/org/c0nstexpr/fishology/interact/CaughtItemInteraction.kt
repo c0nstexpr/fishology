d7bfa3287e639d678010e149e26bdd133bdf34e4
残象之形 <976999484@qq.com>
2023-07-20 17:31:55 +0800

    feat(caught-item): implementing caught item notification

---
+++ src/main/kotlin/org/c0nstexpr/fishology/interact/CaughtItemInteraction.kt
@@ -0,0 +1,11 @@
+package org.c0nstexpr.fishology.interact
+
+import net.minecraft.entity.Entity
+import net.minecraft.entity.ItemEntity
+
+class CaughtItemInteraction {
+
+    fun getItem(entity: Entity){
+        val itemEntity = entity as ItemEntity
+    }
+}
